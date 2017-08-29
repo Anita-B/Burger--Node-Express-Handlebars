fa85a64e4c0c9fa923dcf79d06e6fa13c55a6b8d
Anita Brument <Anita@Anitas-MacBook-Pro.local>
2017-08-28 23:23:08 -0700

    final 1

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, seed) VALUES ('Grilled BBQ chicken burger', 1);
+INSERT INTO burgers (burger_name, seed) VALUES ('Charbroiled Angus beef burger', 1);
+INSERT INTO burgers (burger_name, seed) VALUES ('Turkey with swiss cheese burger', 1);
+INSERT INTO burgers (burger_name, seed) VALUES ('Fried chicken with avocado burger', 1);
+INSERT INTO burgers (burger_name, seed) VALUES ('California Club', 1);
